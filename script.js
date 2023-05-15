1277dfb9311652d67eb1c5c547519d371e27aa8a
Brandios02 <brandon_omar02@hotmail.com>
2023-05-15 16:52:29 -0600

    Actualizacion15/05/23

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ function agregarAlCarrito(precio) {
   carrito.push(precio);
   actualizarCarrito();
 }
-
 // Función que actualiza la lista de productos en el carrito y el total
 function actualizarCarrito() {
   const carritoElemento = document.getElementById("carrito");
   let carritoHTML = "";
-  let total = 0;
+  let subtotal = 0;
   for (let i = 0; i < carrito.length; i++) {
-    total += carrito[i];
+    subtotal += carrito[i];
     carritoHTML += "<li>Producto " + (i+1) + ": $" + carrito[i].toFixed(2) + "</li>";
   }
+  const iva = subtotal * 0.16;
+  const totalConIVA = subtotal + iva;
+  const totalSinIVA = subtotal;
+  carritoHTML += "<li>Subtotal: $" + subtotal.toFixed(2) + "</li>";
+  carritoHTML += "<li>IVA (16%): $" + iva.toFixed(2) + "</li>";
+  carritoHTML += "<li>Total con IVA: $" + totalConIVA.toFixed(2) + "</li>";
   carritoElemento.innerHTML = carritoHTML;
-  document.getElementById("total").innerHTML = "$" + (total * 1.16).toFixed(2);
 }
+
